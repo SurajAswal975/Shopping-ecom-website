454c79948c77b372be78c4975b4fabe9d2fe9dd2
Dileep <exit>
2022-07-13 17:28:40 +0530

    main

---
+++ src/components/discount/Ddata.js
@@ -0,0 +1,48 @@
+const Ddata = [
+  {
+    cover: "./images/discount/discount-1.png",
+    name: "BenuX 2022",
+    price: "INR250",
+  },
+  {
+    cover: "./images/discount/discount-2.png",
+    name: "Sony TV 1080p",
+    price: "INR450",
+  },
+  {
+    cover: "./images/discount/discount-3.png",
+    name: "Sony PS4",
+    price: "INR50",
+  },
+  {
+    cover: "./images/discount/discount-4.png",
+    name: "Setgearr 2022",
+    price: "INR100",
+  },
+  {
+    cover: "./images/discount/discount-5.png",
+    name: "Tony BGB",
+    price: "INR20",
+  },
+  {
+    cover: "./images/discount/discount-6.png",
+    name: "RG products",
+    price: "INR200",
+  },
+  {
+    cover: "./images/discount/discount-7.png",
+    name: "Ranasonic 2022",
+    price: "INR300",
+  },
+  {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "INR30",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "INR80",
+  },
+]
+export default Ddata
